37c26d40ef68648ad8daa1bd4b3e6b8225693cf8
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-05 11:21:22 -0300

    Agregado de array modules y MatButtonModule

--- src/app/utilidades/utilidades.module.ts
+++ src/app/utilidades/utilidades.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 //Angular Material
+import { MatButtonModule } from '@angular/material/button';
+
+const modules = [
+  MatButtonModule
+]
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    ...modules
   ],
   exports: [
-
+    ...modules
   ]
 })
 export class UtilidadesModule { }
