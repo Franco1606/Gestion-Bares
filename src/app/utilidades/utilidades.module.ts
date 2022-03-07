ccb760125bf08860e9bb31c65926386ac7deeb84
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-07 19:51:15 -0300

    Agregue modulos

--- src/app/utilidades/utilidades.module.ts
+++ src/app/utilidades/utilidades.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms"
 
 //Angular Material
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatMenuModule } from "@angular/material/menu"
+import { MatIconModule } from "@angular/material/icon"
+import { MatTableModule } from "@angular/material/table"
+import { MatPaginatorModule } from "@angular/material/paginator"
+import { MatSortModule } from "@angular/material/sort"
+import { MatDialogModule } from "@angular/material/dialog"
 
 const modules = [
-  MatButtonModule
+  MatInputModule,
+  MatFormFieldModule,
+  ReactiveFormsModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatDialogModule
 ]
 
 
