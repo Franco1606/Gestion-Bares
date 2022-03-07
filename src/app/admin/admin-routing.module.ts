2b0f1c90d6d4510433615dbc002f98bcc13cff86
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-07 20:00:55 -0300

    Cree las rutas para logeo y para lista-categorias

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import { LoginAdminComponent } from "./plantillas/login-admin/login-admin.component"
+import { ListaCategoriasComponent } from "./vistas/lista-categorias/lista-categorias.component"
+
+const routes: Routes = [
+  { path: "login", component:LoginAdminComponent },
+  { path: "lista-categorias", component:ListaCategoriasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule]  
 })
 export class AdminRoutingModule { }
