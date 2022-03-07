84f4db4e49991b441a9a73eec747e3951bd1276e
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-07 19:57:17 -0300

    Importe modulos y declare componentes

--- src/app/cocina/cocina.module.ts
+++ src/app/cocina/cocina.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlantillasModule } from "../plantillas/plantillas.module"
 
 import { CocinaRoutingModule } from './cocina-routing.module';
+import { LoginCocinaComponent } from './plantillas/login-cocina/login-cocina.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginCocinaComponent
+  ],
   imports: [
     CommonModule,
-    CocinaRoutingModule
+    CocinaRoutingModule,
+    PlantillasModule
   ]
 })
 export class CocinaModule { }
