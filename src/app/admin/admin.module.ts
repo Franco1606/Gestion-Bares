0881f043d2187f1a622f07b50ad006a0eda09a46
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-07 20:00:31 -0300

    Importe modulos y declare componentes

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,14 +1,30 @@
+//Modulos
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
+import { UtilidadesModule } from "../utilidades/utilidades.module"
+import { PlantillasModule } from "../plantillas/plantillas.module"
 
+//Componentes
+import { LoginAdminComponent } from './plantillas/login-admin/login-admin.component';
+import { ListaCategoriasComponent } from './vistas/lista-categorias/lista-categorias.component';
+import { HeaderAdminComponent } from './plantillas/header-admin/header-admin.component';
+import { AgregarCategoriaDialogComponent } from './Dialogs/agregar-categoria-dialog/agregar-categoria-dialog.component';
+import { EliminarCategoriaDialogComponent } from './Dialogs/eliminar-categoria-dialog/eliminar-categoria-dialog.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginAdminComponent,
+    ListaCategoriasComponent,
+    HeaderAdminComponent,
+    AgregarCategoriaDialogComponent,
+    EliminarCategoriaDialogComponent      
+  ],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    UtilidadesModule,
+    PlantillasModule
+  ]  
 })
 export class AdminModule { }
