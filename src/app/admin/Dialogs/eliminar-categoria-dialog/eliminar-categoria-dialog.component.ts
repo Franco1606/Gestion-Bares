2aea45b2d5c8e010ee05ac66b6333188beafd8e7
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-07 19:59:49 -0300

    Cree el dialog para eliminar una categoria

---
+++ src/app/admin/Dialogs/eliminar-categoria-dialog/eliminar-categoria-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-categoria-dialog',
+  templateUrl: './eliminar-categoria-dialog.component.html',
+  styleUrls: ['./eliminar-categoria-dialog.component.css']
+})
+export class EliminarCategoriaDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
