461075fa49f1eff09be3b93da3bc72570a0dd72a
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-07 19:58:37 -0300

    Cree el login para el admin

---
+++ src/app/admin/plantillas/login-admin/login-admin.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AppService } from "../../../servicios/api/app.service"
+import { Router } from "@angular/router"
+
+@Component({
+  selector: 'app-login-admin',
+  templateUrl: './login-admin.component.html',
+  styleUrls: ['./login-admin.component.css']
+})
+export class LoginAdminComponent implements OnInit {
+
+  constructor( private _api:AppService, private _router:Router ) { }
+
+  errorMsg!:string
+  titulo = "Administrador"
+
+  ngOnInit(): void {
+  }
+
+  logIn(loginData:any) {
+    //Armo body para post en api (campo ususario diferente para cada uno (admin, mozo, cocina))
+    let body = {
+      usuarioAdmin : loginData["usuario"],
+      password : loginData["password"]
+    }
+    this._api.auth(body).subscribe({
+      next: (x) => {
+        if(x.status == "ok") {
+          localStorage.setItem("tokenAdmin", x.result.token)
+          this._router.navigateByUrl("admin/lista-categorias");
+        } else {
+          this.errorMsg = x.result["error_msg"];            
+        }       
+      }, 
+      error: (err) => {console.log(err)}      
+    })    
+  }
+}
