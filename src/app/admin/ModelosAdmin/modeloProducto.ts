e454905a9b4d03c9f3457b246d3a8f30b771dc61
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-07 19:59:06 -0300

    Cree el modelo de producto

---
+++ src/app/admin/ModelosAdmin/modeloProducto.ts
@@ -0,0 +1,9 @@
+export interface modeloProducto {
+    productoID:number,
+    nombre:string,
+    descripcion:string,
+    precio:number,
+    mostrar:number,
+    usuarioID:number,
+    categoriaID:number
+}
