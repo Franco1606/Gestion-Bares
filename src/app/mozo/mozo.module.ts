a4e0d0ecfe56147a66002599e7ac4f04977756c8
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-07 19:55:01 -0300

    importe modulos y declare componentes

--- src/app/mozo/mozo.module.ts
+++ src/app/mozo/mozo.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlantillasModule } from "../plantillas/plantillas.module"
 
 import { MozoRoutingModule } from './mozo-routing.module';
+import { LoginMozoComponent } from './plantillas/login-mozo/login-mozo.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginMozoComponent
+  ],
   imports: [
     CommonModule,
-    MozoRoutingModule
+    MozoRoutingModule,
+    PlantillasModule
   ]
 })
 export class MozoModule { }
