c4810dc57a9b0d8f6400d4532f9c4dad33af7cdd
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-08 12:47:13 -0300

    Modifique el header para traer datos de usuario

--- src/app/admin/plantillas/header-admin/header-admin.component.ts
+++ src/app/admin/plantillas/header-admin/header-admin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+// Inyecciones de dependencia
+import { AppService } from "../../../servicios/api/app.service"
 import { Router } from "@angular/router"
 
 @Component({
@@ -8,12 +10,42 @@ import { Router } from "@angular/router"
 })
 export class HeaderAdminComponent implements OnInit {
 
-  constructor( private _router:Router ) { }
+  constructor( private _router:Router, private _appServiceApi:AppService ) { }
 
-  @Input() usuario!:string
-  @Input() usuarioID!:number 
+  //////////   Atributos de la clase   /////////////  
+  tokenAdmin!:string
+  usuario!:string
+
+  @Output() usuarioID = new EventEmitter<number>();  
 
   ngOnInit(): void {
+    if(localStorage.getItem("tokenAdmin")){
+      this.tokenAdmin = localStorage.getItem("tokenAdmin") || ""
+      this.obtenerUsuario()      
+    } else {
+      alert("Sesion expirada")
+      this._router.navigateByUrl("admin/login")
+    }
+  }
+
+  obtenerUsuario(){    
+    this._appServiceApi.obtenerUsuarioPorToken("tokenAdmin", this.tokenAdmin).subscribe({
+      next: (x) => {
+        if(x.usuarioID != null) {
+          this.usuario = x.usuario
+          this.usuarioID.emit(x.usuarioID)          
+        } else {
+          alert("Sesion expirada")
+          localStorage.removeItem("tokenAdmin")       
+          this._router.navigateByUrl("admin/login")
+        }                 
+      },
+      error: () => {
+        alert("Sesion expirada")
+        localStorage.removeItem("tokenAdmin")       
+        this._router.navigateByUrl("admin/login")
+      }
+    })
   }
 
   closeSesion(){ 
