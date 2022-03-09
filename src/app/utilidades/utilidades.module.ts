dab411f755d6d450c92e51c5138d60f6b92f3fb5
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-09 12:36:18 -0300

    Agregue modulo del matcheckbox

--- src/app/utilidades/utilidades.module.ts
+++ src/app/utilidades/utilidades.module.ts
@@ -14,6 +14,7 @@ import { MatTableModule } from "@angular/material/table"
 import { MatPaginatorModule } from "@angular/material/paginator"
 import { MatSortModule } from "@angular/material/sort"
 import { MatDialogModule } from "@angular/material/dialog"
+import { MatCheckboxModule } from "@angular/material/checkbox"
 
 const modules = [
   MatInputModule,
@@ -27,7 +28,8 @@ const modules = [
   MatTableModule,
   MatPaginatorModule,
   MatSortModule,
-  MatDialogModule
+  MatDialogModule,
+  MatCheckboxModule
 ]
 
 
