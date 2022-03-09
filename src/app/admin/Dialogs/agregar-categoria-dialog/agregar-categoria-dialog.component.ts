cdad661149723198e497c2b364a1f81fba8b2a01
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-09 12:42:22 -0300

    Modifique para enviar el form completo

--- src/app/admin/Dialogs/agregar-categoria-dialog/agregar-categoria-dialog.component.ts
+++ src/app/admin/Dialogs/agregar-categoria-dialog/agregar-categoria-dialog.component.ts
@@ -3,8 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { AdminService } from "../../servicios/api/admin.service"
 // Dependencias Formulario
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-// Dependencia para navegar a otra url
-import { Router } from "@angular/router"
 
 @Component({
   selector: 'app-agregar-categoria-dialog',
@@ -21,7 +19,9 @@ export class AgregarCategoriaDialogComponent implements OnInit {
   tokenAdmin!:string   
   // Formulario //
   form:FormGroup = new FormGroup({
-    "nombre" : new FormControl("", Validators.required)
+    "nombre" : new FormControl("", Validators.required),
+    "usuarioID" : new FormControl(),
+    "tokenAdmin" : new FormControl()
   })
 
   ngOnInit(): void {
@@ -29,23 +29,44 @@ export class AgregarCategoriaDialogComponent implements OnInit {
   }
 
   obtenerDatos() {
-    this.usuarioID = this._AdminServiceApi.usuarioID    
-    this.tokenAdmin = this._AdminServiceApi.tokenAdmin
+    this.form.controls["usuarioID"].setValue(this._AdminServiceApi.usuarioID)
+    this.form.controls["tokenAdmin"].setValue(this._AdminServiceApi.tokenAdmin)
   }
 
   agregarCategoria() {    
     this.nombre = this.form.controls["nombre"].value.trim()
-    this._AdminServiceApi.agregarCategoria(this.usuarioID, this.tokenAdmin, this.nombre).subscribe({
-      next: () => {
-        alert(`Se agrego la categoria: ${this.nombre}`)
-        location.reload()
-      },  
-      error: (err) => {
-        console.log(err)
-        alert(`No se pudo agregar la categoria: ${this.nombre}`)
-        location.reload()
-      }
-    })
+    if(this.verificarCampos(this.nombre)) {
+      this.quitarEspaciosFinales(this.form.value)
+      this._AdminServiceApi.agregarCategoria(this.form.value).subscribe({
+        next: () => {
+          alert(`Se agrego la categoria: ${this.nombre}`)
+          location.reload()
+        },  
+        error: (err) => {
+          console.log(err)
+          alert(`No se pudo agregar la categoria: ${this.nombre}`)
+          location.reload()
+        }
+      })
+    } else {
+      alert("El campo de categoria es requerido")
+    }
   }
 
+  verificarCampos(...campos:string[]):boolean {
+    let verificador = true
+    campos.forEach(campo => {
+      if(campo.trim() == "") {
+        verificador = false
+      }
+    });
+    return verificador
+  }
+
+  quitarEspaciosFinales(json:any){
+    let keys = Object.keys(json);
+    keys.forEach(key => {
+      json[`${key}`] = json[`${key}`].trim()
+    });    
+  }
 }
