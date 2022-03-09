931635e33a7f31848b0e65b9ba8cb11e6cc378d2
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-09 12:41:59 -0300

    Cree el dialog agregar producto

---
+++ src/app/admin/Dialogs/agregar-producto-dialog/agregar-producto-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AgregarProductoDialogComponent } from './agregar-producto-dialog.component';
+
+describe('AgregarProductoDialogComponent', () => {
+  let component: AgregarProductoDialogComponent;
+  let fixture: ComponentFixture<AgregarProductoDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AgregarProductoDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AgregarProductoDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/admin/Dialogs/agregar-producto-dialog/agregar-producto-dialog.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+// Inyecciones de dependencia
+import { AdminService } from "../../servicios/api/admin.service"
+// Dependencias Formulario
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-agregar-producto-dialog',
+  templateUrl: './agregar-producto-dialog.component.html',
+  styleUrls: ['./agregar-producto-dialog.component.css']
+})
+export class AgregarProductoDialogComponent implements OnInit {
+
+  constructor( private _AdminServiceApi:AdminService ) { }
+
+  //////////   Atributos de la clase   /////////////  
+  usuarioID!:number
+  categoriaID!:number
+  tokenAdmin!:string   
+  // Formulario //
+  form:FormGroup = new FormGroup({
+    "nombre" : new FormControl("", Validators.required),
+    "descripcion" : new FormControl(),
+    "precio" : new FormControl("", Validators.required),
+    "usuarioID" : new FormControl(),
+    "categoriaID" : new FormControl(),
+    "tokenAdmin" : new FormControl()
+  })
+
+  ngOnInit(): void {
+    this.obtenerDatos()
+  }
+
+  obtenerDatos() {
+    this.form.controls["usuarioID"].setValue(this._AdminServiceApi.usuarioID)
+    this.form.controls["categoriaID"].setValue(this._AdminServiceApi.categoriaID)
+    this.form.controls["tokenAdmin"].setValue(this._AdminServiceApi.tokenAdmin)
+  }
+
+  agregarProducto() {    
+    let nombre = this.form.controls["nombre"].value
+    let precio = this.form.controls["precio"].value    
+    if(this.validarString(nombre, precio)) {
+      this.quitarEspaciosFinales(this.form.value)
+      this._AdminServiceApi.agregarProducto(this.form.value).subscribe({
+        next: () => {
+          alert(`Se agrego el producto: ${this.form.controls["nombre"].value}`)
+          location.reload()
+        },  
+        error: (err) => {
+          console.log(err)
+          alert(`No se pudo agregar el producto: ${this.form.controls["nombre"].value}`)
+          location.reload()
+        }
+      })
+    } else {
+      alert("Los campos nombre y precio son requeridos")
+    }
+  }
+
+  validarString(...campos:string[]):boolean {
+    let verificador = true
+    campos.forEach(campo => {
+      if(campo.trim() == "") {
+        verificador = false
+      }
+    });
+    return verificador
+  }
+
+  quitarEspaciosFinales(json:any){
+    let keys = Object.keys(json);
+    keys.forEach(key => {
+      if(typeof json[`${key}`] == 'string') {
+        json[`${key}`] = json[`${key}`].trim()
+      }
+    });    
+  }
+}
