0187a8238fbd34e3f15d401aeec8fcdbc8452c78
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-10 18:04:19 -0300

    Importe modulos y declare compoenntes

--- src/app/carta/carta.module.ts
+++ src/app/carta/carta.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UtilidadesModule } from "../utilidades/utilidades.module"
 
 import { CartaRoutingModule } from './carta-routing.module';
+import { CartaComponent } from './Vistas/carta/carta.component';
+import { ColapsadorComponent } from './Plantillas/colapsador/colapsador.component';
+import { ContadorComponent } from './Plantillas/contador/contador.component';
+import { ComentarioDialogComponent } from './Dialogs/comentario-dialog/comentario-dialog.component';
+import { QuitarDialogComponent } from './Dialogs/quitar-dialog/quitar-dialog.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CartaComponent,
+    ColapsadorComponent,
+    ContadorComponent,
+    ComentarioDialogComponent,
+    QuitarDialogComponent    
+  ],
   imports: [
     CommonModule,
-    CartaRoutingModule
+    CartaRoutingModule,
+    UtilidadesModule
   ]
 })
 export class CartaModule { }
