aace319ce19f27de7e94e2600889c80f28bb4c1b
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-10 18:01:53 -0300

    Cree el servicio para la carta

---
+++ src/app/carta/servicios/api/carta.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { Observable } from 'rxjs';
+//Importacion de Modelos
+import { modeloProductoPedido } from "../../ModelosCarta/modeloProductoPedido"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartaService {
+
+  constructor( private _http:HttpClient ) { }
+
+  //URL de la API
+  url = "https://visita360.hopto.org/Api-Gestion-Bares/"
+  //Paso de variables por servicio
+  comentario!:string
+  producto!:modeloProductoPedido
+  productoID!:number
+  IDinterno!:number
+  nombre!:string
+  pedido!:modeloProductoPedido[]
+
+  /////////////////////////////////////////////////////////
+  /////////////  METODOS PARA PRODUCTOS  /////////////////
+  ///////////////////////////////////////////////////////
+
+  //Obtener productos por usuarioID y categoriaID
+  obtenerProductos(usuarioID:number, categoriaID:number):Observable<modeloProductoPedido[]> {
+    return this._http.get<modeloProductoPedido[]>(this.url + `productos.php?usuarioID=${usuarioID}&categoriaID=${categoriaID}`)
+  }
+}
+
+  
