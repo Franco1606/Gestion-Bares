fed4159ad617a2155ed6a28ec13b19d1b77a3b12
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-10 18:02:32 -0300

    Genere el compoenente para el colpasador

---
+++ src/app/carta/Plantillas/colapsador/colapsador.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ColapsadorComponent } from './colapsador.component';
+
+describe('ColapsadorComponent', () => {
+  let component: ColapsadorComponent;
+  let fixture: ComponentFixture<ColapsadorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ColapsadorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ColapsadorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/carta/Plantillas/colapsador/colapsador.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-colapsador',
+  templateUrl: './colapsador.component.html',
+  styleUrls: ['./colapsador.component.css']
+})
+export class ColapsadorComponent {
+
+  constructor() { }
+
+  //////////   Atributos de la clase   /////////////
+  @Input() colorBtnColapsar!:string
+  @Input() titulo!:string
+  @Output() colapsador = new EventEmitter<boolean>();
+  colapsar = true
+
+  conmutar() {
+    this.colapsar = !this.colapsar
+    this.colapsador.emit(this.colapsar)
+  }
+}
