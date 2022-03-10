4478566fd4b41137bd8bccb725db01a8740a35b2
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-10 18:02:49 -0300

    Cree el modelo de producto pedido

---
+++ src/app/carta/ModelosCarta/modeloProductoPedido.ts
@@ -0,0 +1,12 @@
+export interface modeloProductoPedido {
+    productoID:number,
+    nombre:string,
+    descripcion:string,
+    precio:number,
+    mostrar:number,
+    usuarioID:number,
+    categoriaID:number,
+    cantidad:number,
+    comentario:string,
+    IDinterno:number
+}
