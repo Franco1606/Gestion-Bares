b0f0c6663a9939da2516b13d8395113fe049a4cf
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-11 12:43:23 -0300

    Cree el dialog pedido

---
+++ src/app/carta/Dialogs/pedido-dialog/pedido-dialog.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+// Inyecciones de dependencia
+import { CartaService } from "../../servicios/api/carta.service"
+// Modelos
+import { modeloProductoPedido } from '../../ModelosCarta/modeloProductoPedido';
+// Dependencias del Dialog 
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-pedido',
+  templateUrl: './pedido-dialog.component.html',
+  styleUrls: ['./pedido-dialog.component.css']
+})
+export class PedidoDialogComponent implements OnInit {
+
+  constructor( private _cartaServiceApi:CartaService,  private dialogRef:MatDialogRef<PedidoDialogComponent> ) { }
+
+    //////////   Atributos de la clase   /////////////
+    total = 0
+    // Tabla //
+    displayedColumns = ["cantidad", "producto", "precio", "comentario", "eliminar"]
+    dataSource!:modeloProductoPedido[]
+
+  ngOnInit(): void {     
+    this.dataSource = this._cartaServiceApi.pedido    
+    this._cartaServiceApi.pedido.forEach(element => {
+      this.total += element.cantidad*element.precio
+    })
+  }
+
+  quitarProducto(IDinterno:number) {
+    this._cartaServiceApi.pedido = this._cartaServiceApi.pedido.filter(element => element.IDinterno != IDinterno )
+    this.dataSource = this._cartaServiceApi.pedido
+    if(this._cartaServiceApi.pedido.length == 0) {
+      this.dialogRef.close()
+    }
+  }
+
+  emitirOrden() {
+
+  }
+
+}
