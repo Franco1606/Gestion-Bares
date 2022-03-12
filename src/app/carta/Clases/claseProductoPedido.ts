40fe09680a594c705ff8c63e8e30abb6ff531ee3
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-12 17:40:11 -0300

    Elimine atributos de la clase innecesarios

--- src/app/carta/Clases/claseProductoPedido.ts
+++ src/app/carta/Clases/claseProductoPedido.ts
@@ -5,11 +5,7 @@ export class claseProductoPedido {
     constructor(producto:modeloProductoPedido) { 
         this.productoID = producto.productoID
         this.nombre = producto.nombre
-        this.descripcion = producto.descripcion
-        this.precio = producto.precio
-        this.mostrar = producto.mostrar
-        this.usuarioID = producto.usuarioID
-        this.categoriaID = producto.categoriaID
+        this.precio = Number(producto.precio)
         if(!producto.cantidad) {
             this.cantidad = 1    
         } else {
@@ -20,11 +16,7 @@ export class claseProductoPedido {
     //////////   Atributos de la clase   /////////////
     productoID!:number
     nombre!:string
-    descripcion!:string
     precio!:number
-    mostrar!:number
-    usuarioID!:number
-    categoriaID!:number
     cantidad!:number
     comentario!:string
     IDinterno!:number    
