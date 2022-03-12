090c54231921d9c01fb03c7823e46c153509d92d
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-12 17:39:40 -0300

    Agregue dos metodos distintos para generarOrden (domicilio y mesa)

--- src/app/carta/Dialogs/pedido-dialog/pedido-dialog.component.ts
+++ src/app/carta/Dialogs/pedido-dialog/pedido-dialog.component.ts
@@ -17,27 +17,56 @@ export class PedidoDialogComponent implements OnInit {
 
     //////////   Atributos de la clase   /////////////
     total = 0
+    domicilio!:string
     // Tabla //
     displayedColumns = ["cantidad", "producto", "precio", "comentario", "eliminar"]
     dataSource!:claseProductoPedido[]
 
   ngOnInit(): void {     
-    this.dataSource = this._cartaServiceApi.pedido
-    this._cartaServiceApi.pedido.forEach(element => {
+    this.dataSource = this._cartaServiceApi.pedidos
+    this._cartaServiceApi.pedidos.forEach(element => {
       this.total += element.cantidad*element.precio
     })
   }
 
   quitarProducto(IDinterno:number) {
-    this._cartaServiceApi.pedido = this._cartaServiceApi.pedido.filter(element => element.IDinterno != IDinterno )
-    this.dataSource = this._cartaServiceApi.pedido
-    if(this._cartaServiceApi.pedido.length == 0) {
+    this.total -= this._cartaServiceApi.pedidos.filter(element => element.IDinterno == IDinterno )[0].precio
+    this._cartaServiceApi.pedidos = this._cartaServiceApi.pedidos.filter(element => element.IDinterno != IDinterno )
+    this.dataSource = this._cartaServiceApi.pedidos
+
+    if(this._cartaServiceApi.pedidos.length == 0) {
       this.dialogRef.close()
     }
   }
 
-  emitirOrden() {
-
+  emitirOrden() {    
+    if(this._cartaServiceApi.mesaID == null) {
+      this.domicilio = prompt("Ingrese su domicilio") || ""
+      this._cartaServiceApi.generarOrdenDomicilio(this._cartaServiceApi.usuarioID, this.domicilio, this._cartaServiceApi.pedidos, "nueva", "cliente").subscribe({
+        next: (x) => {
+          console.log(x.result["ordenID"])
+          console.log(x.result["nuevaFecha"])
+          console.log(x.result["numOrden"])
+          location.reload()
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
+    } else {
+      console.log(this._cartaServiceApi.mesaID)
+      this._cartaServiceApi.generarOrdenMesa(this._cartaServiceApi.usuarioID, this._cartaServiceApi.mesaID, this._cartaServiceApi.pedidos, "nueva", "cliente").subscribe({
+        next: (x) => {
+          console.log(x.result["ordenID"])
+          console.log(x.result["nuevaFecha"])
+          console.log(x.result["numOrden"])
+          location.reload()
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
+    }    
   }
 
 }
