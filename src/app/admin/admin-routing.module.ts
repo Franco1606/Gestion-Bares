805b6890ee5026aceefbec6b7efc03729dd1624b
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-14 09:58:17 -0300

    Agregue las rutas a inicio y estilo

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 //Componentes
 import { LoginAdminComponent } from "./plantillas/login-admin/login-admin.component"
+import { InicioComponent } from "./vistas/inicio/inicio.component"
 import { ListaCategoriasComponent } from "./vistas/lista-categorias/lista-categorias.component"
 import { ListaProductosComponent } from "./vistas/lista-productos/lista-productos.component"
+import { EstilosCartaComponent } from "./vistas/estilos-carta/estilos-carta.component"
 
 const routes: Routes = [
   { path: "login", component:LoginAdminComponent },
+  { path: "inicio", component:InicioComponent },
   { path: "lista-categorias", component:ListaCategoriasComponent },
-  { path: "lista-productos/:categoriaID", component:ListaProductosComponent }
+  { path: "lista-productos/:categoriaID", component:ListaProductosComponent },
+  { path: "estilos-carta", component:EstilosCartaComponent }
 ];
 
 @NgModule({
