deb0b25fa13d0ae365c816d0a9224cbb2234d284
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-18 17:11:56 -0300

    Agregue el modulo de utilidades

--- src/app/mozo/mozo.module.ts
+++ src/app/mozo/mozo.module.ts
@@ -1,18 +1,25 @@
+//Modulos
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MozoRoutingModule } from './mozo-routing.module';
+import { UtilidadesModule } from "../utilidades/utilidades.module"
 import { PlantillasModule } from "../plantillas/plantillas.module"
 
-import { MozoRoutingModule } from './mozo-routing.module';
 import { LoginMozoComponent } from './plantillas/login-mozo/login-mozo.component';
+import { GestionMesasComponent } from './vistas/gestion-mesas/gestion-mesas.component';
+import { HeaderMozoComponent } from './plantillas/header-mozo/header-mozo.component';
 
 
 @NgModule({
   declarations: [
-    LoginMozoComponent
+    LoginMozoComponent,
+    GestionMesasComponent,
+    HeaderMozoComponent
   ],
   imports: [
     CommonModule,
     MozoRoutingModule,
+    UtilidadesModule,
     PlantillasModule
   ]
 })
