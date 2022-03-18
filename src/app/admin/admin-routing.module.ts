9f26ce80f257d5e21f303b67b085daadab663f52
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-18 01:14:39 -0300

    Agregue la ruta a gestion-mozos

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -7,13 +7,15 @@ import { InicioComponent } from "./vistas/inicio/inicio.component"
 import { ListaCategoriasComponent } from "./vistas/lista-categorias/lista-categorias.component"
 import { ListaProductosComponent } from "./vistas/lista-productos/lista-productos.component"
 import { EstilosCartaComponent } from "./vistas/estilos-carta/estilos-carta.component"
+import { GestionMozosComponent } from "./vistas/gestion-mozos/gestion-mozos.component"
 
 const routes: Routes = [
   { path: "login", component:LoginAdminComponent },
   { path: "inicio", component:InicioComponent },
   { path: "lista-categorias", component:ListaCategoriasComponent },
   { path: "lista-productos/:categoriaID", component:ListaProductosComponent },
-  { path: "estilos-carta", component:EstilosCartaComponent }
+  { path: "estilos-carta", component:EstilosCartaComponent },
+  { path: "gestion-mozos", component:GestionMozosComponent }
 ];
 
 @NgModule({
