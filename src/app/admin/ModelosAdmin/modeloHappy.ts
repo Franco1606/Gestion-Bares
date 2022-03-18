3962c3a55ab865cafef13fa9f1343b21a8fdee6b
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-17 21:39:52 -0300

    Agregue el modeloHappy

---
+++ src/app/admin/ModelosAdmin/modeloHappy.ts
@@ -0,0 +1,18 @@
+export interface modeloHappy {
+    happyID:number,
+    estado:number,
+    inicio:string,
+    fin:string,
+    usuarioID:number,
+    categoriaID:number,
+    lunes:number,
+    martes:number,
+    miercoles:number,
+    jueves:number,
+    viernes:number,
+    sabado:number,
+    domingo:number
+
+
+
+}
