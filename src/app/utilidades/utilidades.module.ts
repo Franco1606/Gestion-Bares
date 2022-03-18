e5d185bb204d6b9558e816edce609ae037773885
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-17 21:37:09 -0300

    Agregue el matSelectModule

--- src/app/utilidades/utilidades.module.ts
+++ src/app/utilidades/utilidades.module.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from "@angular/material/form-field"
 import { MatInputModule } from "@angular/material/input"
 import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatSelectModule } from "@angular/material/select"
 import { MatMenuModule } from "@angular/material/menu"
 import { MatIconModule } from "@angular/material/icon"
 import { MatTableModule } from "@angular/material/table"
@@ -26,6 +27,7 @@ const modules = [
   MatButtonModule,
   MatToolbarModule,
   MatMenuModule,
+  MatSelectModule,
   MatIconModule,
   MatTableModule,
   MatPaginatorModule,
