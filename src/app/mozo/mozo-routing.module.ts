fc446ec25b415d6ff70e8ade27489062e2388e45
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-18 17:12:10 -0300

    Agregue la ruta gestion-mesas

--- src/app/mozo/mozo-routing.module.ts
+++ src/app/mozo/mozo-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 //Componentes
 import { LoginMozoComponent } from "./plantillas/login-mozo/login-mozo.component"
+import { GestionMesasComponent } from "./vistas/gestion-mesas/gestion-mesas.component"
 
 const routes: Routes = [
   { path: "login", component:LoginMozoComponent },
+  { path: "gestion-mesas", component:GestionMesasComponent }
 ];
 
 @NgModule({
