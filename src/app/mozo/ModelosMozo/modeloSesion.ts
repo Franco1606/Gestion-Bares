4676393fd1dd7efc9347a9173b3d6c037eff1fae
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-19 11:03:23 -0300

    Hice el modelo de sesion

---
+++ src/app/mozo/ModelosMozo/modeloSesion.ts
@@ -0,0 +1,12 @@
+export interface modeloSesion {
+    sesionID: number,
+    usuarioID:number,
+    mozoID:number,
+    solicitadaFecha:Date,
+    abiertaFecha:Date,
+    cerradaFecha:Date,
+    estado:string,
+    ordenNueva:number,
+    ordenLista:number,
+    mesaID:number
+}
