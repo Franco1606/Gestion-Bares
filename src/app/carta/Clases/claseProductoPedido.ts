1f44a7098c64ae42d017c83dcb3a26c3334acdb3
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-19 11:06:52 -0300

    Agregue categoriaNombre

--- src/app/carta/Clases/claseProductoPedido.ts
+++ src/app/carta/Clases/claseProductoPedido.ts
@@ -11,16 +11,18 @@ export class claseProductoPedido {
             this.cantidad = 1    
         } else {
             this.cantidad = producto.cantidad
-        }        
+        }
+        this.categoriaNombre = producto.categoriaNombre        
     }
 
     //////////   Atributos de la clase   /////////////
     productoID!:number
     categoriaID!:number
+    categoriaNombre:string
     nombre!:string
     precio!:number
     cantidad!:number
     comentario!:string
-    IDinterno!:number    
+    IDinterno!:number
 }
 
