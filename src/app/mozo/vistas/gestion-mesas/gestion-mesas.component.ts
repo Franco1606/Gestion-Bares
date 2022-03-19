9f517aa4a7dc73fb1db70b1ca8f7f9671d8c50bd
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-19 11:02:14 -0300

    Modifique gestion-mesas

--- src/app/mozo/vistas/gestion-mesas/gestion-mesas.component.ts
+++ src/app/mozo/vistas/gestion-mesas/gestion-mesas.component.ts
@@ -1,20 +1,58 @@
-import { Component} from '@angular/core';
+import { Component, ViewEncapsulation} from '@angular/core';
+//Modelos
+import { modeloSesion } from '../../ModelosMozo/modeloSesion';
+//Inyeccions de dependencia
+import { MoozoService } from "../../servicios/api/moozo.service"
+// Dependencias Angular Material
+import { MatDialog } from "@angular/material/dialog"
+// Dependencias para Dialogs
+import { DetallesSesionDialogComponent } from "../../Dialogs/detalles-sesion-dialog/detalles-sesion-dialog.component"
+
 
 @Component({
   selector: 'app-gestion-mesas',
   templateUrl: './gestion-mesas.component.html',
-  styleUrls: ['./gestion-mesas.component.css']
+  styleUrls: ['./gestion-mesas.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GestionMesasComponent {
 
-  constructor() { }
+  constructor( private _mozoService:MoozoService, private _dialog:MatDialog ) { }
 
-  //////////   Atributos de la clase   /////////////  
+  //////////   Atributos de la clase   /////////////
+  usuarioID:number  
   mozoID!:number   
   tokenMozo!:string
+  sesiones!:modeloSesion[]
 
   obtenerUsuario(datosUsuario:any) {
-    this.mozoID = datosUsuario["mozoID"]    
+    this.usuarioID = datosUsuario["usuarioID"]
+    this.mozoID = datosUsuario["mozoID"]
     this.tokenMozo = datosUsuario["tokenMozo"]
+    this.obtenerSesiones()
+  }
+
+  obtenerSesiones() {
+    this._mozoService.obtenerSesiones(this.usuarioID).subscribe({
+      next: (x) => {
+        this.sesiones = x.filter(sesion => sesion.estado == "solicitada" || (sesion.estado == "abierta" && sesion.mozoID == this.mozoID))
+      }
+    })
+  }
+
+  detallesMesa(sesion:modeloSesion) {
+    this._mozoService.sesion = sesion
+    this._mozoService.usuarioID = this.usuarioID
+    this._mozoService.tokenMozo = this.tokenMozo
+    this._dialog.open(DetallesSesionDialogComponent)
+  }
+
+  tiempoTranscurrido(fecha:Date):string {
+    let ahora = Date.now()
+    let dif = ahora - Date.parse(fecha.toString())    
+    let min = Math.floor((dif / (1000 * 60)) % 60)
+    let horas = Math.floor((dif / (1000 * 60 * 60)) % 24)
+    let string = `${horas} horas, ${min} min`  
+    return string         
   }
 }
