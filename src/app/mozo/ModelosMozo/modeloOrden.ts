d0f53c913d48bd1ab40764f9873edf1416d2f0e9
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-19 11:03:51 -0300

    Hice el modelo de Orden

---
+++ src/app/mozo/ModelosMozo/modeloOrden.ts
@@ -0,0 +1,16 @@
+export interface modeloOrden {
+    ordenID:number,
+    sesionID:number,
+    finalizoMozoID:number,
+    usuarioID:number,
+    mesaID:number,
+    domicilio:string,
+    numOrden:string,
+    nuevaFecha:Date,
+    activaFecha:Date,
+    listaFecha:Date,
+    finalizadaFecha:Date,
+    estado:string,
+    solicitante:string,
+    total:number
+}
