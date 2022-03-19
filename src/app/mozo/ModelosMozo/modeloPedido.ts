448dcdbcd9b402863794cb8149d7bb2f216b24a0
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-19 11:03:38 -0300

    Hice el modelo de pedido

---
+++ src/app/mozo/ModelosMozo/modeloPedido.ts
@@ -0,0 +1,16 @@
+export interface modeloPedido {
+    pedidoID:number,
+    ordenID:number,
+    finalizoMozoID:number,
+    categoriaID:number,
+    productoID:number,
+    usuarioID:number,
+    sesionID:number,
+    cantidad:number,
+    nombre:string,
+    precio:number,
+    cocina:number,
+    comentario:string,
+    happy:number,
+    categoriaNombre:string
+}
