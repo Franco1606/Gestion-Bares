ab910f6bf5704bffaba78d2951e43713095b7d27
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-21 14:57:15 -0300

    Agregue la ruta gestion-pedidos

--- src/app/cocina/cocina-routing.module.ts
+++ src/app/cocina/cocina-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 //Componentes
 import { LoginCocinaComponent } from "./plantillas/login-cocina/login-cocina.component"
+import { GestionPedidosComponent } from "./vistas/gestion-pedidos/gestion-pedidos.component"
 
 const routes: Routes = [
   { path: "login", component:LoginCocinaComponent },
+  { path: "gestion-pedidos", component:GestionPedidosComponent },
 ];
 
 @NgModule({
