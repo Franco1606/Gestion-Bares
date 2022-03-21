705e16636ccf30d5e4d7ad2e2b5c940c9224d506
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-21 14:56:57 -0300

    Importe el moduloe de utilidades

--- src/app/cocina/cocina.module.ts
+++ src/app/cocina/cocina.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlantillasModule } from "../plantillas/plantillas.module"
+import { UtilidadesModule } from "../utilidades/utilidades.module"
 
 import { CocinaRoutingModule } from './cocina-routing.module';
 import { LoginCocinaComponent } from './plantillas/login-cocina/login-cocina.component';
+import { HeaderCocinaComponent } from './plantillas/header-cocina/header-cocina.component';
+import { GestionPedidosComponent } from './vistas/gestion-pedidos/gestion-pedidos.component';
 
 
 @NgModule({
   declarations: [
-    LoginCocinaComponent
+    LoginCocinaComponent,
+    HeaderCocinaComponent,
+    GestionPedidosComponent
   ],
   imports: [
     CommonModule,
     CocinaRoutingModule,
-    PlantillasModule
+    PlantillasModule,
+    UtilidadesModule
   ]
 })
 export class CocinaModule { }
