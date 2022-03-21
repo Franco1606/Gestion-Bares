ca29a81a49e3817c3dafe162d6d61cf2ad9fa81d
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-21 20:17:48 -0300

    Agregue el link a gestion-ordenes

--- src/app/cocina/cocina-routing.module.ts
+++ src/app/cocina/cocina-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 //Componentes
 import { LoginCocinaComponent } from "./plantillas/login-cocina/login-cocina.component"
 import { GestionPedidosComponent } from "./vistas/gestion-pedidos/gestion-pedidos.component"
+import { GestionOrdenesComponent } from "./vistas/gestion-ordenes/gestion-ordenes.component"
 
 const routes: Routes = [
   { path: "login", component:LoginCocinaComponent },
   { path: "gestion-pedidos", component:GestionPedidosComponent },
+  { path: "gestion-ordenes", component:GestionOrdenesComponent }
 ];
 
 @NgModule({
