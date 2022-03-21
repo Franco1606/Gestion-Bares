b4478cbd26e2ee994c7cb6cf8ac355ab5475d063
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-21 20:17:34 -0300

    Se agregaron componentes

--- src/app/cocina/cocina.module.ts
+++ src/app/cocina/cocina.module.ts
@@ -7,13 +7,21 @@ import { CocinaRoutingModule } from './cocina-routing.module';
 import { LoginCocinaComponent } from './plantillas/login-cocina/login-cocina.component';
 import { HeaderCocinaComponent } from './plantillas/header-cocina/header-cocina.component';
 import { GestionPedidosComponent } from './vistas/gestion-pedidos/gestion-pedidos.component';
+import { DetallesPedidoDialogComponent } from './Dialogs/detalles-pedido-dialog/detalles-pedido-dialog.component';
+import { GestionOrdenesComponent } from './vistas/gestion-ordenes/gestion-ordenes.component';
+import { DetallesOrdenDialogComponent } from './Dialogs/detalles-orden-dialog/detalles-orden-dialog.component';
+import { ComentarioDialogComponent } from './Dialogs/comentario-dialog/comentario-dialog.component';
 
 
 @NgModule({
   declarations: [
     LoginCocinaComponent,
     HeaderCocinaComponent,
-    GestionPedidosComponent
+    GestionPedidosComponent,
+    DetallesPedidoDialogComponent,
+    GestionOrdenesComponent,
+    DetallesOrdenDialogComponent,
+    ComentarioDialogComponent
   ],
   imports: [
     CommonModule,
