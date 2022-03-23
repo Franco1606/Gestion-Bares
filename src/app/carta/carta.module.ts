df5862d0e5deecfacdda659c3361aa98edd0467c
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-23 03:48:31 -0300

    Nada importante

--- src/app/carta/carta.module.ts
+++ src/app/carta/carta.module.ts
@@ -6,18 +6,18 @@ import { PlantillasModule } from "../plantillas/plantillas.module"
 import { CartaRoutingModule } from './carta-routing.module';
 import { CartaComponent } from './Vistas/carta/carta.component';
 import { ColapsadorComponent } from './Plantillas/colapsador/colapsador.component';
-import { ComentarioDialogComponent } from './Dialogs/comentario-dialog/comentario-dialog.component';
 import { QuitarDialogComponent } from './Dialogs/quitar-dialog/quitar-dialog.component';
 import { PedidoDialogComponent } from './Dialogs/pedido-dialog/pedido-dialog.component';
+import { ComentarioDialogComponent } from "./Dialogs/comentario-dialog/comentario-dialog.component"
 
 
 @NgModule({
   declarations: [
     CartaComponent,
     ColapsadorComponent,
-    ComentarioDialogComponent,
     QuitarDialogComponent,
-    PedidoDialogComponent    
+    PedidoDialogComponent,
+    ComentarioDialogComponent
   ],
   imports: [
     CommonModule,
