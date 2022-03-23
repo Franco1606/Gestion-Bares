647494d571adebacb1a4f68031cf284a7e8b5730
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-23 00:10:41 -0300

    Importe modulo plantillas para usar el contador

--- src/app/carta/carta.module.ts
+++ src/app/carta/carta.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UtilidadesModule } from "../utilidades/utilidades.module"
+import { PlantillasModule } from "../plantillas/plantillas.module"
 
 import { CartaRoutingModule } from './carta-routing.module';
 import { CartaComponent } from './Vistas/carta/carta.component';
 import { ColapsadorComponent } from './Plantillas/colapsador/colapsador.component';
-import { ContadorComponent } from './Plantillas/contador/contador.component';
 import { ComentarioDialogComponent } from './Dialogs/comentario-dialog/comentario-dialog.component';
 import { QuitarDialogComponent } from './Dialogs/quitar-dialog/quitar-dialog.component';
 import { PedidoDialogComponent } from './Dialogs/pedido-dialog/pedido-dialog.component';
@@ -15,7 +15,6 @@ import { PedidoDialogComponent } from './Dialogs/pedido-dialog/pedido-dialog.com
   declarations: [
     CartaComponent,
     ColapsadorComponent,
-    ContadorComponent,
     ComentarioDialogComponent,
     QuitarDialogComponent,
     PedidoDialogComponent    
@@ -23,7 +22,8 @@ import { PedidoDialogComponent } from './Dialogs/pedido-dialog/pedido-dialog.com
   imports: [
     CommonModule,
     CartaRoutingModule,
-    UtilidadesModule
+    UtilidadesModule,
+    PlantillasModule
   ]
 })
 export class CartaModule { }
