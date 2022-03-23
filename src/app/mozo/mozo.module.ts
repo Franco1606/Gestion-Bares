ff1797e7e0ba3a9acb7c8cef41fe32f657f7b2b8
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-23 00:09:14 -0300

    Se declararon los componentes agregados

--- src/app/mozo/mozo.module.ts
+++ src/app/mozo/mozo.module.ts
@@ -5,13 +5,17 @@ import { MozoRoutingModule } from './mozo-routing.module';
 import { UtilidadesModule } from "../utilidades/utilidades.module"
 import { PlantillasModule } from "../plantillas/plantillas.module"
 
+//Componentes
 import { LoginMozoComponent } from './plantillas/login-mozo/login-mozo.component';
 import { GestionMesasComponent } from './vistas/gestion-mesas/gestion-mesas.component';
 import { HeaderMozoComponent } from './plantillas/header-mozo/header-mozo.component';
 import { DetallesSesionDialogComponent } from './Dialogs/detalles-sesion-dialog/detalles-sesion-dialog.component';
 import { DetallesOrdenComponent } from './Dialogs/detalles-orden/detalles-orden.component';
 import { CerrarMesaDialogComponent } from './Dialogs/cerrar-mesa-dialog/cerrar-mesa-dialog.component';
-
+import { PedidoMozoComponent } from './vistas/pedido-mozo/pedido-mozo.component';
+import { ProductosComponent } from './vistas/productos/productos.component';
+import { AgregarProductoComponent } from './Dialogs/agregar-producto-dialog/agregar-producto-dialog.component';
+import { VerPedidoDialogComponent } from './Dialogs/ver-pedido-dialog/ver-pedido-dialog.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { CerrarMesaDialogComponent } from './Dialogs/cerrar-mesa-dialog/cerrar-m
     HeaderMozoComponent,
     DetallesSesionDialogComponent,
     DetallesOrdenComponent,
-    CerrarMesaDialogComponent
+    CerrarMesaDialogComponent,
+    PedidoMozoComponent,
+    ProductosComponent,
+    AgregarProductoComponent,
+    VerPedidoDialogComponent
   ],
   imports: [
     CommonModule,
