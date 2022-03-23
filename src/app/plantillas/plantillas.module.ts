4e724c1981265a7e8d324177d1491c69b16a0c7d
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-23 03:38:57 -0300

    Nada importante

--- src/app/plantillas/plantillas.module.ts
+++ src/app/plantillas/plantillas.module.ts
@@ -6,7 +6,6 @@ import { UtilidadesModule } from "../utilidades/utilidades.module";
 import { LoginComponent } from './login/login.component';
 import { ContadorComponent } from "./contador/contador.component"
 
-
 @NgModule({
   declarations: [
     LoginComponent,
