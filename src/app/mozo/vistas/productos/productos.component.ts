6a9d9b4db1dddbc115ee4360274f083585d44230
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-23 03:39:18 -0300

    Agregue metodos para agregar producto

--- src/app/mozo/vistas/productos/productos.component.ts
+++ src/app/mozo/vistas/productos/productos.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 // Inyecciones de dependencia
 import { AdminService } from "../../../admin/servicios/api/admin.service"
 import { MoozoService } from "../../servicios/api/moozo.service"
@@ -6,6 +6,7 @@ import { ActivatedRoute, Router } from "@angular/router"
 // Modelos
 import { modeloProducto } from '../../../admin/ModelosAdmin/modeloProducto';
 import { modeloProductoPedido } from 'src/app/carta/ModelosCarta/modeloProductoPedido';
+import { modeloCategoria } from 'src/app/admin/ModelosAdmin/modeloCategoria';
 // Dependencias para la tabla
 import { MatPaginator } from "@angular/material/paginator"
 import { MatTableDataSource } from '@angular/material/table';
@@ -13,7 +14,7 @@ import { MatSort } from '@angular/material/sort';
 // Dependencias Angular Material
 import { MatDialog } from "@angular/material/dialog"
 // Dependencias para Dialogs
-import { AgregarProductoComponent } from "../../Dialogs/agregar-producto-dialog/agregar-producto-dialog.component"
+import { AgregarComentarioComponent } from "../../Dialogs/agregar-comentario-dialog/agregar-comentario-dialog.component"
 import { VerPedidoDialogComponent } from '../../Dialogs/ver-pedido-dialog/ver-pedido-dialog.component';
 //Clases
 import { claseProductoPedido } from 'src/app/carta/Clases/claseProductoPedido';
@@ -23,7 +24,7 @@ import { claseProductoPedido } from 'src/app/carta/Clases/claseProductoPedido';
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {  
+export class ProductosComponent implements OnInit{  
 
   constructor( private _adminService:AdminService, private _mozoService:MoozoService, private _route:ActivatedRoute, private _dialog:MatDialog ) { }
 
@@ -31,21 +32,26 @@ export class ProductosComponent {
   usuarioID!:number
   mozoID!:number
   tokenMozo!:string
+  categoria!:modeloCategoria
   categoriaID!:number 
   productos!:modeloProducto[]
   pedido:claseProductoPedido[] = []
+  IDinterno = 0 
   // Tabla //
-  displayedColumns = ["nombre"]
+  displayedColumns = ["nombre", "accion"]
   @ViewChild(MatPaginator) paginator!:MatPaginator
   @ViewChild(MatSort) sort!: MatSort;
   dataSource!:MatTableDataSource<modeloProducto>
+
+  ngOnInit(): void {
+    this.obtenerDatos()
+  }
 
   obtenerUsuario(datosUsuario:any) {
     this.usuarioID = datosUsuario["usuarioID"]
     this.mozoID = datosUsuario["mozoID"]
     this.tokenMozo = datosUsuario["tokenMozo"]
     this.pedido = this._mozoService.pedido
-    this.obtenerDatos()
     this.obtenerProductos()    
   }
 
@@ -58,6 +64,7 @@ export class ProductosComponent {
     this._adminService.obtenerCategoria(this.categoriaID).subscribe({
       next: (x) => {
         this._mozoService.categoria = x
+        this.categoria = x
       },
       error: (err) => {
         alert("No se pudo obtener los datos de la base de datos")
@@ -81,11 +88,6 @@ export class ProductosComponent {
     })
   }
 
-  irAgregar(producto:modeloProductoPedido) {    
-    this._mozoService.producto = producto
-    this._dialog.open(AgregarProductoComponent)
-  }
-
   verPedido() {
     this._mozoService.usuarioID = this.usuarioID
     this._mozoService.mozoID = this.mozoID
@@ -96,6 +98,37 @@ export class ProductosComponent {
         this.pedido = this._mozoService.pedido
       }
     })
+  }
+
+  pasarCantidad(e:number, productoPedido:modeloProductoPedido) {
+    productoPedido.cantidad = e
+  }
+
+  agregar(producto:modeloProductoPedido, comentario:number) {    
+    if(comentario == 1) {      
+      let dialogRef = this._dialog.open(AgregarComentarioComponent)
+      this._mozoService.pedido = this.pedido
+      this._mozoService.producto = producto
+      this._mozoService.IDinterno = this.IDinterno
+      dialogRef.afterClosed().subscribe({
+        next: () => {
+          this.pedido = this._mozoService.pedido
+          this.IDinterno = this._mozoService.IDinterno        
+        }
+      })
+      console.log(this._mozoService.pedido)
+
+    } else {
+      if( this.pedido.filter(element =>  element.productoID == producto.productoID && element.comentario == null).length == 0 ) {      
+        let prod = new claseProductoPedido(producto)
+        prod.IDinterno = this.IDinterno
+        this.pedido.push(prod)
+        this.IDinterno += 1
+      } else {
+        alert("Este producto ya se agrego al pedido")
+      }
+      console.log(this._mozoService.pedido)
+    }
   }
 
   //Filtro para el buscador de la tabla
