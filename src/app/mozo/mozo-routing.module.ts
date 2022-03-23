0889d59c680aaa24be51fcd45a6885a99f5010c1
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-23 00:09:23 -0300

    Agregue rutas

--- src/app/mozo/mozo-routing.module.ts
+++ src/app/mozo/mozo-routing.module.ts
@@ -4,10 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 //Componentes
 import { LoginMozoComponent } from "./plantillas/login-mozo/login-mozo.component"
 import { GestionMesasComponent } from "./vistas/gestion-mesas/gestion-mesas.component"
+import { PedidoMozoComponent } from "./vistas/pedido-mozo/pedido-mozo.component"
+import { ProductosComponent } from "./vistas/productos/productos.component"
 
 const routes: Routes = [
   { path: "login", component:LoginMozoComponent },
-  { path: "gestion-mesas", component:GestionMesasComponent }
+  { path: "gestion-mesas", component:GestionMesasComponent },
+  { path: "pedido-mozo", component:PedidoMozoComponent },
+  { path: "productos/:categoriaID", component:ProductosComponent }
 ];
 
 @NgModule({
