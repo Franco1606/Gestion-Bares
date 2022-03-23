7e5e3e82aa0f21317e4be03faa98314901c0c0fe
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-23 00:04:38 -0300

    declare y exporte ContadorComponent

--- src/app/plantillas/plantillas.module.ts
+++ src/app/plantillas/plantillas.module.ts
@@ -4,18 +4,21 @@ import { UtilidadesModule } from "../utilidades/utilidades.module";
 
 //Componentes
 import { LoginComponent } from './login/login.component';
+import { ContadorComponent } from "./contador/contador.component"
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    ContadorComponent
   ],
   imports: [
     CommonModule,
     UtilidadesModule
   ],
   exports: [
-    LoginComponent
+    LoginComponent,
+    ContadorComponent
   ]
 })
 export class PlantillasModule { }
