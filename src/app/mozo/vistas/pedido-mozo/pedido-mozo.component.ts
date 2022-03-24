d1f5dea942a0c7b4f8e1553ab299f0d81bb72d57
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-24 00:21:26 -0300

    Agregue mesaID e IDinterno

--- src/app/mozo/vistas/pedido-mozo/pedido-mozo.component.ts
+++ src/app/mozo/vistas/pedido-mozo/pedido-mozo.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 // Inyecciones de dependencia
 import { AdminService } from "../../../admin/servicios/api/admin.service"
 import { MoozoService } from "../../servicios/api/moozo.service"
@@ -19,7 +19,7 @@ import { VerPedidoDialogComponent } from "../../Dialogs/ver-pedido-dialog/ver-pe
   templateUrl: './pedido-mozo.component.html',
   styleUrls: ['./pedido-mozo.component.css']
 })
-export class PedidoMozoComponent {
+export class PedidoMozoComponent implements OnInit{
 
   constructor( private _router:Router, private _adminService:AdminService, private _mozoService:MoozoService, private _dialog:MatDialog ) { }
 
@@ -29,17 +29,24 @@ export class PedidoMozoComponent {
   tokenMozo!:string
   categorias!:modeloCategoria[]
   pedido:claseProductoPedido[] = []
+  mesaID!:number
+  IDinterno!:number
   // Tabla //
   displayedColumns = ["nombre"]
   @ViewChild(MatPaginator) paginator!:MatPaginator
   @ViewChild(MatSort) sort!: MatSort;
   dataSource!:MatTableDataSource<modeloCategoria>
 
+  ngOnInit(): void {
+    this.mesaID = this._mozoService.mesaID
+  }
+
   obtenerUsuario(datosUsuario:any) {
     this.usuarioID = datosUsuario["usuarioID"]
     this.mozoID = datosUsuario["mozoID"]
     this.tokenMozo = datosUsuario["tokenMozo"]
     this.pedido = this._mozoService.pedido
+    this.verificarIDinterno()
     this.obtenerCategorias()
   }  
 
@@ -72,6 +79,22 @@ export class PedidoMozoComponent {
         this.pedido = this._mozoService.pedido
       }
     })
+  }
+
+  verificarIDinterno() { 
+    if(this.pedido.length){
+      let mayor
+      for (let i = 0; i < this.pedido.length-1; i++) {
+        if(this.pedido[i].IDinterno > this.pedido[i+1].IDinterno) {
+          mayor = this.pedido[i].IDinterno
+        } else {
+          mayor = this.pedido[i+1].IDinterno
+        }
+      }
+      this.IDinterno = mayor + 1     
+    } else {
+      this.IDinterno = 0
+    }
   }
 
   //Filtro para el buscador de la tabla
