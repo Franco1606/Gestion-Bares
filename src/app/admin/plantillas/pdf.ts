1312c433614fe7ce4fb60a9dd0f9fc95eb689217
Bootcamp Redjar <frankobertolotti@gmail.com>
2022-03-26 02:13:52 -0300

    Modificacion para imprimir por comandera

--- src/app/admin/plantillas/pdf.ts
+++ src/app/admin/plantillas/pdf.ts
@@ -2,67 +2,95 @@ import { modeloPedido } from "../../mozo/ModelosMozo/modeloPedido";
 import { DatePipe } from '@angular/common';
 
 export class Pdf {
-    //////////   Atributos de la clase   /////////////
-    
-    crear(pedidos:modeloPedido[], numOrden:string):any {
-        const datepipe: DatePipe = new DatePipe('en-US')
-        let formattedDate = datepipe.transform(new Date(), 'dd MMM YYYY, HH:mm')
-        let body:any = []
-        body[0] = ["Cantidad", "Producto", "Comentarios"]            
+    //////////   Atributos de la clase   /////////////    
+    crear(pedidos:modeloPedido[], numOrden:string):any {        
+        let comanderas:number[] = []
         pedidos.forEach(pedido => {
-            let cantidad
-            if(pedido.cantidad == 0.5) {
-                cantidad = "1/2"
-            } else if(pedido.cantidad >= 1) {
-                cantidad = pedido.cantidad
+            if(!comanderas.includes(pedido.comandera) && Number(pedido.comandera)) {
+                comanderas.push(pedido.comandera)
             }
-            let fila = [cantidad, pedido.nombre, pedido.comentario]
-            body.push(fila)
-        })            
+        })        
+        let content:any[] = []
 
-        let contenido:any = {
+        comanderas.forEach(comandera => {
+            const datepipe: DatePipe = new DatePipe('en-US')
+            let formattedDate = datepipe.transform(new Date(), 'dd MMM YYYY, HH:mm')
+            let body:any = []
+            let pedidosComanda = pedidos.filter(element => element.comandera == comandera)
+            body[0] = ["Cantidad", "Producto", "Comentarios"]
+            pedidosComanda.forEach(pedidoComanda => {
+            let cantidad
+            
+            if(pedidoComanda.cantidad == 0.5) {
+                cantidad = "1/2"
+            } else if(pedidoComanda.cantidad >= 1) {
+                cantidad = pedidoComanda.cantidad
+            }
+
+            let fila = [cantidad, pedidoComanda.nombre, pedidoComanda.comentario]
+            body.push(fila)
+            })            
+
+            let tituloComandera = {         
+                text: `Comandera: ${comandera}`,
+                alignment: 'center',
+                margin: [0,0,0,10],
+                fontSize: 14,                
+            }
+
+            let fechaImpresa = {
+                text: `Fecha: ${formattedDate}`,
+                alignment: 'center',
+                margin: [0,0,0,10],
+                fontSize: 13
+            }
+
+            let numeroDeOrden = {
+                text: `Orden #: ${numOrden}`,
+                alignment: 'center',
+                margin: [0,0,0,10],
+                fontSize: 13
+            }
+
+            let tablaConPedidos = {                
+                columns: [
+                    { width: 0, text: '' },
+                    {
+                        layout: 'lightHorizontalLines', 
+                        width: 'auto',
+                        alignment: 'center',                                       
+                        table: {
+                            headerRows: 1,
+                            widths: [ 'auto', 'auto', 'auto'],              
+                            body: body,
+                            margin: [0,20,0,0],                    
+                        }
+                    },
+                    { width: 0, text: '' },
+                ]                
+            }
+
+            let separador = {
+                text: '',
+                margin: [0,0,0,50]
+            }
+
+            
+            content.push(tituloComandera)
+            content.push(fechaImpresa)
+            content.push(numeroDeOrden)
+            content.push(tablaConPedidos)
+            content.push(separador)
+           
+        })
+
+        let contenido:any = {                       
             pageSize: {
-                width: 250,
-                height: 'auto'
+                width: 300,
+                height: 'auto'                
             },
-            pageMargins: [ 10, 20, 10, 0 ],
-            content: [ 
-                {
-                    text: `Comandera: ${pedidos[0].comandera}`,
-                    alignment: 'center',
-                    margin: [0,0,0,10],
-                    fontSize: 14,
-                },               
-                {
-                    text: `Fecha: ${formattedDate}`,
-                    alignment: 'center',
-                    margin: [0,0,0,10],
-                    fontSize: 10,
-                },
-                {
-                    text: `Orden #: ${numOrden}`,
-                    alignment: 'center',
-                    margin: [0,0,0,10],
-                    fontSize: 10,
-                },
-                {
-                    columns: [
-                        { width: 0, text: '' },
-                        {
-                            layout: 'lightHorizontalLines', 
-                            width: "auto",
-                            alignment: 'center',                                       
-                            table: {
-                                headerRows: 1,
-                                widths: [ 'auto', 'auto', 'auto'],              
-                                body: body,                      
-                                margin: [0,20,0,0],                    
-                            }
-                        },
-                        { width: 0, text: '' },
-                    ]                                      
-                }               
-            ]
+            pageMargins: [ 10, 20, 10, 20 ],
+            content: content           
         }
         
         return contenido
